6207fe6afdc2c22c870a61d4289330c3f6b8f366
Elvis Salvatierra <esalvatierra@aeromexico.com>
2023-07-13 13:18:20 -0500

    * se modifica el test para utilizar junit5 ya que ese es que el usa el proyecto. * se modifica el cast de getId en el test, porque se espera un elemento unico pero se intenta transformar a una lista. * se elimina el tema de public porque esto es un test.

---
+++ src/test/java/com/personal/veterinaria/service/ConsultaServicioTest.java
@@ -0,0 +1,14 @@
+package com.personal.veterinaria.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsultaServicioTest {
+
+
+    @Test
+    void getAll() {
+    }
+}
